5937a5ac5d1e083a5119161fc23cbc20c8f39a4e
Jasonjspark <52211447+Jasonjspark@users.noreply.github.com>
2020-07-22 20:19:41 +0900

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,1 @@
+/home/coa/PhoenixMiner_5.0e_Linux/PhoenixMiner -pool 175.201.47.66:8008 -wal 0xb5bc91e14e2b986ac97cccc261ebfedca9306fd7.0999 -pass zil1usydn4pr9rmxyqaazlekn54hg5xl74mtue0fx0@asia1-etc.ethermine.org:14444@4G -pool2 asia1-etc.ethermine.org:14444 -worker 0999 -proto 2 -proto2 3 -log 0 -wdog 0 -clKernel 1 -clGreen 1 -amd -rvram -1 -eres 0 -tstop 90 -fret 2 -ftimeout 120
